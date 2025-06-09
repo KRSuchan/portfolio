8d41ae269e0ac01c7bfbb91c83fb00fff28c6782
KRSuchan <lsc1814@naver.com>
2025-06-09 17:06:49 +0900

    Fix webpack build errors and update dependencies

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: "export",
+    basePath: "/portfolio",
+    images: {
+        unoptimized: true,
+    },
+    webpack: (config) => {
+        config.resolve.fallback = {
+            ...config.resolve.fallback,
+            fs: false,
+        };
+        return config;
+    },
+};
+
+module.exports = nextConfig;
